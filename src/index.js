6c2b2425af11c944838cc7fb87e12382ef45b2f9
Vinicius Gama Sá <vinicinho.gama@hotmail.com.br>
2023-04-19 21:15:36 -0300

    more fixes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 
@@ -6,7 +6,7 @@ const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
